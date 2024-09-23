14c821dc4d8c5129af29bf44630541ec7b82b009
William Santana de Amorim <williamsantanadeamorim@MacBook-Air-de-William.local>
2024-09-22 21:04:23 -0400

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace ProductApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private const string SessionKey = "Products";
+
+        public List<string> Products { get; set; } = new List<string>();
+
+        [BindProperty]
+        public string NewProduct { get; set; }
+
+        public void OnGet()
+        {
+            var productsData = HttpContext.Session.GetString(SessionKey);
+            if (productsData != null)
+            {
+                Products = JsonSerializer.Deserialize<List<string>>(productsData);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            var productsData = HttpContext.Session.GetString(SessionKey);
+            if (productsData != null)
+            {
+                Products = JsonSerializer.Deserialize<List<string>>(productsData);
+            }
+
+            if (!string.IsNullOrWhiteSpace(NewProduct))
+            {
+                Products.Add(NewProduct);
+            }
+
+            HttpContext.Session.SetString(SessionKey, JsonSerializer.Serialize(Products));
+
+            return RedirectToPage();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
+
